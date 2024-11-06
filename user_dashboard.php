84fdca2ab761b3d5f9a8359c1720c8f8f647c1c2
Bond, James Bond Inc. <lucas.l@bondjamesbondinc.com>
2024-11-06 08:23:08 -0500

    edit button

--- user_dashboard.php
+++ user_dashboard.php
@@ -105,13 +105,9 @@ $conn->close();
     </main>
 
     <!-- Footer Section -->
-    <footer>
-        <p>&copy; 2024 Rookies 2.0 | All rights reserved.</p>
-        <div class="footer-links">
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms of Service</a>
-        </div>
-    </footer>
+    <footer >
+        <?php include 'footer.php'; ?>
+    </foote>
 </body>
 
 </html>
