049ec1292724cf6ad114286c854ad2ab1e878f91
Bond, James Bond Inc. <lucas.l@bondjamesbondinc.com>
2024-11-10 08:19:48 -0500

    deleted inapropriate html

--- create_listing.php
+++ create_listing.php
@@ -111,16 +111,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 
 <body>
-    <div id="successModal" style="display: none;">
-        <div class="modal-content">
-            <h2>Listing Created Successfully!</h2>
-            <p>Choose your next action:</p>
-            <button onclick="window.location.href='listings.php'">View All Listings</button>
-            <button onclick="window.location.href='user_dashboard.php'">View My Listings</button>
-            <button onclick="window.location.href='create_listing.php'">Create New Listing</button>
-            <button onclick="closeSuccessModal()">Close</button>
-        </div>
-    </div>
 
 
 
