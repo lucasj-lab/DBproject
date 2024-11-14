97cf1a67cd747d26949e03dbdc56647b5ca668b0
Bond, James Bond Inc. <lucas.l@bondjamesbondinc.com>
2024-11-13 19:50:01 -0500

    modified:   database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -2,7 +2,7 @@
 // Database connection settings
 $servername = "127.0.0.1"; 
 $username = "root";        
-$password = "";          
+$password = "admin";          
 $dbname = "projectdb";     
 $port = "3306";  //
 
