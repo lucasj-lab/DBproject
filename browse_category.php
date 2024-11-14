8ef4604e752e1c61aa28dce3e3812f52c6bc8203
Bond, James Bond Inc. <lucas.l@bondjamesbondinc.com>
2024-11-14 05:18:13 -0500

    modified:   browse_category.php 	modified:   styles.css

--- browse_category.php
+++ browse_category.php
@@ -71,12 +71,6 @@ $conn->close();
         <?php endif; ?>
     </main>
 
-    <footer>
-        <p>&copy; 2024 Rookies 2.0 | All rights reserved.</p>
-        <div class="footer-links">
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms of Service</a>
-        </div>
-    </footer>
+    <?php include 'footer.php'; ?>
 </body>
 </html>
