bd2923cb5164e2e7a68dd1772576ccf4cde61d94
Bond, James Bond Inc. <lucas.l@bondjamesbondinc.com>
2024-11-17 06:22:36 -0500

    modified:   listings.php

--- listings.php
+++ listings.php
@@ -1,6 +1,9 @@
 <?php
 require 'database_connection.php';
 require 'listing_queries.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['fetchListings'])) {
     try {
