cc08f6f54c77d6dc66a99ca7973cddc1534f0d5e
Bond, James Bond Inc. <lucas.l@bondjamesbondinc.com>
2024-11-27 03:27:38 -0500

    fixed html tag

--- view_message.php
+++ view_message.php
@@ -48,7 +48,7 @@ if ($message['Recipient_ID'] === $userId) {
 }
 ?>
 <!DOCTYPE html>
-< lang="en">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
