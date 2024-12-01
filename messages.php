b20f8e0c9781583b457e65a6d4dff22d8d4cd9c9
Bond, James Bond Inc. <lucas.l@bondjamesbondinc.com>
2024-12-01 15:54:26 -0500

    removed hamburger icon

--- messages.php
+++ messages.php
@@ -18,12 +18,7 @@ $section = $_GET['section'] ?? 'inbox';
     <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
 </head>
 <body>
-    <div class="main-header">
-        <div class="hamburger-menu">
-            <svg class="hamburger-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path d="M3 6h18M3 12h18M3 18h18"></path>
-            </svg>
-        </div>
+
         <div class="search-container">
             <div class="search-input-container">
                 <input type="text" class="search-input" placeholder="Search messages...">
