289d1147abdc53ac4900153c0196f5fbb6ad15f3
Bond, James Bond Inc. <lucas.l@bondjamesbondinc.com>
2024-12-07 00:52:23 -0500

    database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "database-1-instance-1.ctkqko2k074a.us-east-2.rds.amazonaws.com";
-$username = "admin";
-$password = "Butterball3!";
+$servername = "Joshuas-PC";
+$username = "root";
+$password = "";
 $dbname = "projectDB";
 
 try {
